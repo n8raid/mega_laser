d789a7ffdf9cf3d4d5a087b16d08ab0bd5955a9b
Blizz_Roach <96886173+Reeconator@users.noreply.github.com>
2025-05-22 23:00:06 -0600

    reece work on player pt 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,20 @@
 extends Node2D
+@export var MOVE_SPEED = 250
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var joy1_x = Input.get_axis("move_left", "move_right")
+	print(joy1_x)
+	if joy1_x == 0:
+		pass
+	if joy1_x < 0:
+		MOVE_SPEED = 200
+	if joy1_x > 0:
+		MOVE_SPEED = 375
+	position.x += MOVE_SPEED * delta * joy1_x
