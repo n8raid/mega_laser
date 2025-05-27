ef127d3343464562c06037bc627070d6f91aa16d
Blizz_Roach <96886173+Reeconator@users.noreply.github.com>
2025-05-26 23:24:39 -0600

    Gun rotation implementation
    Fully implemented gun rotation on r-joy movement.
    Partially implemented Player upgrade logic and gun logic.
    Currently changing upgrade amounts does not seem to actually have an effect in the debug print outs, but I'm unsure as to why. Will focus on creating a working base gun before implementing upgrades.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,72 @@
 extends Node2D
-@export var MOVE_SPEED = 250
+
+
+@export var move_speed_up = 0
+@export var gun_damage_up = 0
+@export var gun_speed_up = 0
+@export var gun_spread_up = 0
+
+
+var MOVE_SPEED = 287.5 + (move_speed_up * 25)
+var GUN_DAMAGE = 1 + (gun_damage_up)
+var GUN_SPEED = 0.3 - (gun_speed_up * 0.05)
+var GUN_SPREAD = 1 + (gun_spread_up)
+
+
+
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$GunTimer.wait_time = GUN_SPEED
+	$GunTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	
+	## Movement, faster to right than left
 	var joy1_x = Input.get_axis("move_left", "move_right")
-	print(joy1_x)
 	if joy1_x == 0:
 		pass
 	if joy1_x < 0:
 		MOVE_SPEED = 200
 	if joy1_x > 0:
-		MOVE_SPEED = 375
+		MOVE_SPEED = 325
 	position.x += MOVE_SPEED * delta * joy1_x
+
+
+	## Gun controls (rotation)
+	var joy2 = Input.get_vector("shoot_left", "shoot_right", "shoot_up","shoot_down")
+	if joy2.length() > 0.1:
+		if joy2.y >= 0: # cancels movement under the y-axis
+			joy2.y = 0
+			
+		var joy2_rad = atan2(joy2.y,joy2.x)
+		var joy2_rad_center = joy2_rad + (PI/2)
+		$Gun.rotation = joy2_rad_center
+		
+	
+		
+		
+
+	## Gun firing
+func _on_gun_timer_timeout() -> void:
+	var joy2 = Input.get_vector("shoot_left", "shoot_right", "shoot_up","shoot_down")
+	if joy2.length() > 0.1:
+		if joy2.y >= 0: # cancels movement under the y-axis
+			joy2.y = 0
+	if joy2.length() > 0.1:
+		print(GUN_DAMAGE)
+		print(GUN_SPREAD)
+		print(GUN_SPEED)
+		print(MOVE_SPEED)
+		shoot()
+	# GunTimer speed update
+	$GunTimer.wait_time = GUN_SPEED
+		
+
+## Bullet logic - will implement later
+func shoot() -> void: 
+	pass
